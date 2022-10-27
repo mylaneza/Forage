80de66203baa249aa51a585558cdf9e86e7cbf5e
Ignacio Gonzalez <mylaneza@gmail.com>
2022-10-26 21:10:40 -0500

    Set up the project to use Room

--- app/src/main/java/com/example/forage/data/ForageableDao.kt
+++ app/src/main/java/com/example/forage/data/ForageableDao.kt
@@ -16,6 +16,12 @@
 package com.example.forage.data
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.forage.model.Forageable
+import kotlinx.coroutines.flow.Flow
 
 /**
  * Data Access Object for database interaction.
@@ -24,13 +30,19 @@ import androidx.room.Dao
 interface ForageableDao {
 
     // TODO: implement a method to retrieve all Forageables from the database
-
+    @Query("SELECT * FROM forageable_database")
+    fun getForageables() : Flow<List<Forageable>>
     // TODO: implement a method to retrieve a Forageable from the database by id
-
+    @Query("SELECT * FROM forageable_database WHERE id = :id")
+    fun getForageable(id : Long) : Flow<Forageable>
     // TODO: implement a method to insert a Forageable into the database
+    @Insert
+    suspend fun insert(forageable: Forageable)
     //  (use OnConflictStrategy.REPLACE)
-
     // TODO: implement a method to update a Forageable that is already in the database
-
+    @Update
+    suspend fun update(forageable: Forageable)
     // TODO: implement a method to delete a Forageable from the database.
+    @Delete
+    suspend fun delete(forageable: Forageable)
 }
